59ac5528eab346f499ef1bdf8573659935bc1e44
prathiksha-12 <pratikshar20041@gmail.com>
2025-04-23 12:12:59 +0530

    Update videos_Summary_Final.py

--- videos_Summary_Final.py
+++ videos_Summary_Final.py
@@ -86,7 +86,7 @@ def turn_into_story(summary):
 
 
 st.title("PragyanAI - Youtube/Uploaded Video Summarizer Using Groq LLM")
-#st.image("PragyanAI_Transparent.png")
+st.image("icons.jpg")
 
 youtube_url = st.text_input("Paste a Youtube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
